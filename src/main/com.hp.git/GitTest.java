d2c692146d0df189f8360d909dc7a02f9cb95ffe
Huping Chen <18327184833@163.com>
2025-03-24 14:03:58 +0800

    正常合并提交提交

--- src/main/com.hp.git/GitTest.java
+++ src/main/com.hp.git/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hell Git");
         System.out.println("二次提交");
+        System.out.println("正常合并");
     }
 }
